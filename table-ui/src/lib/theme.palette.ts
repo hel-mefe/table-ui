f1bbcfe4f93168080a123b8b62267c687325eb4d
hel-mefe <elmefeddelhicham@gmail.com>
2026-03-14 01:39:29 +0000

    feat: add a color palette for the app

---
+++ table-ui/src/lib/theme.palette.ts
@@ -0,0 +1,22 @@
+export const palette = {
+  primary: {
+    main: "#2F80ED",
+    hover: "#1C6DD0",
+    light: "#E8F1FD"
+  },
+
+  background: {
+    app: "#F4F7F9",
+    sidebar: "#F2F4F7",
+    card: "#D1D5DB"
+  },
+
+  text: {
+    primary: "#2F3A4A",
+    secondary: "#687582"
+  },
+
+  border: {
+    default: "#D0D5DD"
+  }
+}
